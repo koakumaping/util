05663c4807abb0101dc702307f4e877ab1b818d0
pingyf <pingyf@fangde.com>
2016-04-01 14:38:22 +0800

    增加是否为PC端判断函数

--- util.js
+++ util.js
@@ -27,5 +27,34 @@
 
     var util;
 
+    util = {
+
+    	/**
+		* 判断是否是PC端
+		*
+		* 用于判断当前浏览器是否是PC端
+		*
+		* @returns true  是PC端
+		* @returns false 不是PC端
+		*
+		* @date 2016-04-01
+		* @author Ping YF (koakumaping@163.com)
+		*/
+    	isPC: function () {
+    		var userAgentInfo = navigator.userAgent;
+	        var Agents = ["Android", "iPhone",
+	                    "SymbianOS", "Windows Phone",
+	                    "iPad", "iPod"];
+	        var flag = true;
+	        for (var v = 0; v < Agents.length; ++v) {
+	            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+	                flag = false;
+	                break;
+	            }
+	        }
+	        return flag;
+    	}
+    }
+
     return util;
 });
