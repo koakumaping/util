60f13dc4e3ce5b4499e8b5c4d5df690eddea1437
Ping-YF <koakumaping@163.com>
2016-04-12 10:34:38 +0800

    增加uri解析

--- util.js
+++ util.js
@@ -101,6 +101,67 @@
             return px / fontSize;
         },
 
+        /**
+        * URI解析
+        *
+        * 获取URI中的参数
+        *
+        * @returns get String
+        * @returns keys Array
+        * @returns Count Number
+        *
+        * @date 2016-04-12
+        * @author Ping YF (koakumaping@163.com)
+        */
+        uri: function (search) {
+            var s = search || location.search,
+                reg = /([?&])?([^=]+?)(?=(=|&|$))(([^&$]*))?/g,
+                r = {},
+                match = null,
+                total = 0;
+            var _remove = function(key) {
+                // r[key] = undefined;
+                delete r[key];
+                total--;
+            };
+
+            while(match = reg.exec(s)){
+                var val = decodeURIComponent(match[4]).replace(/^=/, "");
+                if (match[2].indexOf('[]') !== -1) {
+                    var k = match[2].replace('[]', '');
+                    if (typeof r[k] === 'undefined') {
+                        r[k] = [val];
+                        total++;
+                    } else {
+                        r[k].push(val);
+                    }
+                } else {
+                    r[match[2]] = val;
+                    total++;
+                }
+            }
+
+            return {
+                get: function(key) {
+                    return r[key];
+                },
+                keys: function() {
+                    var keys = [];
+                    if ('keys' in Object) {
+                        keys = Object.keys(r);
+                    } else {
+                        for (var key in r) {
+                            keys.push(key);
+                        }
+                    }
+                    return keys;
+                },
+                remove: _remove,
+                count: function() {
+                    return total;
+                }
+            };
+        }
         
     }
 
